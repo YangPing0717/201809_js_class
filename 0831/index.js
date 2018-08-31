8f0cc7d55c6160c8d55f9aca07f99c3a6be34fb8
YangPing0717 <office.yangping717@gmail.com>
2018-08-31 22:02:41 +0800

    Add files via upload

---
+++ 0831/index.js
@@ -0,0 +1,33 @@
+// var number = 5;
+// var name = 'AAA';
+// console.log(name);
+// document.write(name);
+
+// function addNumber(x, y) {
+//     var sum = x + y;
+//     return sum;
+// }
+// // 呼叫使用函式
+// var a = 1;
+// var b = 2;
+// var sum = addNumber(a, b);
+// console.log(sum);
+
+// var position = 0;
+
+// function moveTo(distance) {
+//     position = position + distance;
+// }
+// console.log("原:", position)
+// moveTo(100);
+// console.log("移:", position)
+
+
+
+var str = '1';
+var num = 1;
+var result = str + num;
+console.log(result, '型態是', typeof result);
+
+var x = 2 / 1;
+console.log(isNaN(x));
